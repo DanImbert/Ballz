d37553e2ee019083a6786b3a0fd08b40c3cf85f8
Daniel Imbert <danielimbert90@gmail.com>
2022-05-05 04:03:11 +0300

    Made a mess scaled it up

--- Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
+++ Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
@@ -117,7 +117,7 @@ namespace UnityEditor.Rendering.Universal
             // we want the fade group to stay hidden.
             using (var group = new EditorGUILayout.FadeGroupScope(1.0f - m_AnimDirOptions.faded))
                 if (group.visible)
-                    { var dr_120_40_637872002285827073 = m_AnimAreaOptions.target; settings.DrawRange(); }
+                    { var dr_120_40_637873048499558340 = m_AnimAreaOptions.target; settings.DrawRange(); }
 
             // Spot angle
             using (var group = new EditorGUILayout.FadeGroupScope(m_AnimSpotOptions.faded))
